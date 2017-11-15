7c6dc47bf61c95e3e5856c027660f2d964404aa8
zhangyikun <1251134350@qq.com>
2017-11-15 17:01:58 +0800

    编写Web框架，和配置文件

--- apis.py
+++ apis.py
@@ -21,3 +21,23 @@ class APIError(Exception):
         self.data = data
         self.message = message
 
+class APIValueError(APIError):
+    '''
+    Indicate the input value has error or invalid. The data specifies the error field of input form.
+    '''
+    def __init__(self, field, message=''):
+        super(APIValueError, self).__init__('value:invalid', field, message)
+
+class APIResourceNotFoundError(APIError):
+    '''
+    Indicate the resource was not found. The data specifies the resource name.
+    '''
+    def __init__(self, field, message=''):
+        super(APIResourceNotFoundError, self).__init__('value:notfound', field, message)
+
+class APIPermissionError(APIError):
+    '''
+    Indicate the api has no permission.
+    '''
+    def __init__(self, message=''):
+        super(APIPermissionError, self).__init__('permission:forbidden', 'permission', message)
---
+++ config_default.py
@@ -0,0 +1,14 @@
+#config_default.py
+configs = {
+    'db': {
+        'host' : '127.0.0.1',
+        'post' : 3306,
+        'user' : 'www-data',
+        'passowrd' : 'www-data',
+        'database' : 'awesome'
+    },
+    'session' : {
+        'secret' : 'AweEs0mE'
+    }
+
+}
